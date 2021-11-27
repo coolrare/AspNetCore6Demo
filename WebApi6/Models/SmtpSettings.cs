7fd90c9ecbc3adb3de34944c7d168e24deda8fbd
Will 保哥 <doggy.huang@gmail.com>
2021-11-27 10:49:30 +0800

    示範 services.Configure<T> 與 IOptions<T> 組態設定與注入方法

---
+++ WebApi6/Models/SmtpSettings.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace WebApi6.Models
+{
+    public class SmtpSettings
+    {
+        public string Host { get; set; }
+        public string User { get; set; }
+        public string Pass { get; set; }
+    }
+}
--- WebApi6/Program.cs
+++ WebApi6/Program.cs
@@ -1,15 +1,57 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebApi6.Helpers;
 using WebApi6.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddSingleton<JwtHelpers>();
+
+builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
+
+builder.Services.AddControllers(options =>
+{
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // 當驗證失敗時，回應標頭會包含 WWW-Authenticate 標頭，這裡會顯示失敗的詳細錯誤原因
+        options.IncludeErrorDetails = true; // 預設值為 true，有時會特別關閉
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // 透過這項宣告，就可以從 "sub" 取值並設定給 User.Identity.Name
+            NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+            // 透過這項宣告，就可以從 "roles" 取值，並可讓 [Authorize] 判斷角色
+            RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+
+            // 一般我們都會驗證 Issuer
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("JwtSettings:Issuer"),
+
+            // 通常不太需要驗證 Audience
+            ValidateAudience = false,
+            //ValidAudience = "JwtAuthDemo", // 不驗證就不需要填寫
+
+            // 一般我們都會驗證 Token 的有效期間
+            ValidateLifetime = true,
+
+            // 如果 Token 中包含 key 才需要驗證，一般都只有簽章而已
+            ValidateIssuerSigningKey = false,
+
+            // "1234567890123456" 應該從 IConfiguration 取得
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JwtSettings:SignKey")))
+        };
+    });
 
 builder.Services.AddDbContext<ContosouniversityContext>(options =>
 {
@@ -20,7 +62,11 @@ builder.Services.AddDbContext<ContosouniversityContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseDeveloperExceptionPage();
+//}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -29,6 +75,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
